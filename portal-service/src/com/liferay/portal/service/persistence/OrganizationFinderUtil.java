8d7df25b71a332969306d9a4385e223f8b21bdb0
arbetts <andrew.betts@liferay.com>
2014-05-12 14:04:47 -0700

    LPS-46658 build-services

--- portal-service/src/com/liferay/portal/service/persistence/OrganizationFinderUtil.java
+++ portal-service/src/com/liferay/portal/service/persistence/OrganizationFinderUtil.java
@@ -92,6 +92,12 @@ public class OrganizationFinderUtil {
 		return getFinder().findByNoAssets();
 	}
 
+	public static java.util.List<com.liferay.portal.model.Organization> findByG_U(
+		long groupId, long userId)
+		throws com.liferay.portal.kernel.exception.SystemException {
+		return getFinder().findByG_U(groupId, userId);
+	}
+
 	public static java.util.List<java.lang.Long> findByC_P(long companyId,
 		long parentOrganizationId, long previousOrganizationId, int size)
 		throws com.liferay.portal.kernel.exception.SystemException {
