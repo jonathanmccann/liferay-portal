1e16ff23a65df0d02462633fbc009de226246b0f
Preston-Crary <preston.crary@liferay.com>
2014-05-28 17:44:45 -0700

    LPS-46353 Handle adding and removing UserGroupRoles separately

--- portal-service/src/com/liferay/portlet/usersadmin/util/UsersAdmin.java
+++ portal-service/src/com/liferay/portlet/usersadmin/util/UsersAdmin.java
@@ -132,6 +132,10 @@ public interface UsersAdmin {
 	public List<UserGroupRole> getUserGroupRoles(PortletRequest portletRequest)
 		throws PortalException, SystemException;
 
+	public List<UserGroupRole> getUserGroupRoles(
+			PortletRequest portletRequest, boolean add)
+		throws PortalException, SystemException;
+
 	public List<UserGroup> getUserGroups(Hits hits)
 		throws PortalException, SystemException;
 
