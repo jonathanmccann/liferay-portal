88066dcfdd6cfde47d30a0b8f33cb1c1864c1eca
Preston-Crary <preston.crary@liferay.com>
2014-11-12 16:56:43 -0800

    LPS-51401 Throw an IllegalArguementException instead of allowing the NullPointerException possibility

--- portal-impl/src/com/liferay/portlet/dynamicdatamapping/util/DDMImpl.java
+++ portal-impl/src/com/liferay/portlet/dynamicdatamapping/util/DDMImpl.java
@@ -113,6 +113,12 @@ public class DDMImpl implements DDM {
 			refererPortletName = serviceContext.getPortletId();
 		}
 
+		if (refererPortletName == null) {
+			throw new IllegalArgumentException(
+				"ServiceContext must have either refererPortletName or " +
+					"portletPreferencesIds set.");
+		}
+
 		return DDMDisplayRegistryUtil.getDDMDisplay(refererPortletName);
 	}
 
