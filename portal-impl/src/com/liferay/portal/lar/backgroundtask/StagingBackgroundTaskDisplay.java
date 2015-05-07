f3943436e9a4e78c2fe24fe064782feeb4a4d209
Jonathan McCann <jonathan.mccann@liferay.com>
2015-05-07 13:50:55 -0700

    LPS-54744 Change logic to follow original logic in JSP

--- portal-impl/src/com/liferay/portal/lar/backgroundtask/StagingBackgroundTaskDisplay.java
+++ portal-impl/src/com/liferay/portal/lar/backgroundtask/StagingBackgroundTaskDisplay.java
@@ -250,17 +250,6 @@ public class StagingBackgroundTaskDisplay extends BaseBackgroundTaskDisplay {
 
 	protected String processMessage(Locale locale) {
 
-		String messageKey = "exporting";
-
-		if (Validator.equals(_cmd, Constants.IMPORT)) {
-			messageKey = "importing";
-		}
-		else if (Validator.equals(_cmd, Constants.PUBLISH_TO_LIVE) ||
-				 Validator.equals(_cmd, Constants.PUBLISH_TO_REMOTE)) {
-
-			messageKey = "publishing";
-		}
-
 		String message = StringPool.BLANK;
 
 		if (hasRemoteMessage()) {
@@ -268,8 +257,18 @@ public class StagingBackgroundTaskDisplay extends BaseBackgroundTaskDisplay {
 				locale,
 				"please-wait-as-the-publication-processes-on-the-remote-site");
 		}
+		else if (hasStagedModelMessage()) {
+			String messageKey = "exporting";
 
-		if (hasStagedModelMessage()) {
+			if (Validator.equals(_cmd, Constants.IMPORT)) {
+				messageKey = "importing";
+			}
+			else if (Validator.equals(_cmd, Constants.PUBLISH_TO_LIVE) ||
+					 Validator.equals(_cmd, Constants.PUBLISH_TO_REMOTE)) {
+
+				messageKey = "publishing";
+			}
+
 			StringBundler sb = new StringBundler();
 
 			sb.append("<strong>");
