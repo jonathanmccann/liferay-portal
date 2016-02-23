8f5e929164bd925bab8d3d3b27c20f50766c415f
dsanz <daniel.sanz@liferay.com>
2016-02-23 19:59:21 +0100

    LPS-63584 Document S3Store configuration

--- modules/apps/foundation/portal-store/portal-store-s3/src/main/java/com/liferay/portal/store/s3/configuration/S3StoreConfiguration.java
+++ modules/apps/foundation/portal-store/portal-store-s3/src/main/java/com/liferay/portal/store/s3/configuration/S3StoreConfiguration.java
@@ -28,49 +28,83 @@ import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClass
 )
 public interface S3StoreConfiguration {
 
-	@Meta.AD(required = false)
+	@Meta.AD(
+		description = "%access-key-help", name = "%access-key", required = false
+	)
 	public String accessKey();
 
-	@Meta.AD(required = false)
+	@Meta.AD(
+		description = "%secret-key-help", name = "%secret-key",
+		required = false
+	)
 	public String secretKey();
 
-	@Meta.AD(deflt = "us-east-1", required = false)
+	@Meta.AD(
+		deflt = "us-east-1", description = "%s3-region-help",
+		name = "%s3-region", required = false
+	)
 	public String s3Region();
 
-	@Meta.AD(required = true)
+	@Meta.AD(
+		description = "%bucket-name-help", name = "%bucket-name",
+		required = true)
 	public String bucketName();
 
-	@Meta.AD(deflt = "7", required = false)
-		public int corePoolSize();
+	@Meta.AD(
+		deflt = "7", description = "%core-pool-size-help",
+		name = "%core-pool-size", required = false
+	)
+	public int corePoolSize();
 
-	@Meta.AD(deflt = "5", required = false)
+	@Meta.AD(
+		deflt = "50", description = "%http-client-max-connections-help",
+		name = "%http-client-max-connections", required = false
+	)
+	public int httpClientMaxConnections();
+
+	@Meta.AD(
+		deflt = "5", description = "%http-client-max-error-retry-help",
+		name = "%http-client-max-error-retry", required = false
+	)
 	public int httpClientMaxErrorRetry();
 
-	@Meta.AD(deflt = "20", required = false)
+	@Meta.AD(
+		deflt = "20", description = "%max-pool-size-help",
+		name = "%max-pool-size", required = false
+	)
 	public int maxPoolSize();
 
-	@Meta.AD(deflt = "5242880", required = false)
+	@Meta.AD(
+		deflt = "5242880", description = "%minimum-uploads-part-size-help",
+		name = "%minimum-uploads-part-size", required = false)
 	public int minimumUploadPartSize();
 
-	@Meta.AD(deflt = "10485760", required = false)
+	@Meta.AD(
+		deflt = "10485760", description = "%multipart-upload-threshold-help",
+		name = "%multipart-upload-threshold", required = false
+	)
 	public int multipartUploadThreshold();
 
 	@Meta.AD(
-		deflt = "STANDARD", optionValues = {"REDUCED_REDUNDANCY", "STANDARD"},
-		required = false
+		deflt = "STANDARD", description = "%s3-storage-class-help",
+		name = "%s3-storage-class",
+		optionValues = {"REDUCED_REDUNDANCY", "STANDARD"}, required = false
 	)
 	public String s3StorageClass();
 
 	@Meta.AD(deflt = "10000", required = false)
 	public int connectionTimeout();
 
-	@Meta.AD(deflt = "50", required = false)
-	public int httpClientMaxConnections();
-
-	@Meta.AD(deflt = "7", required = false)
+	@Meta.AD(
+		deflt = "7", description = "%cache-dir-clean-up-expunge-help",
+		name = "%cache-dir-clean-up-expunge", required = false
+	)
 	public int cacheDirCleanUpExpunge();
 
-	@Meta.AD(deflt = "100", required = false)
+	@Meta.AD(
+		deflt = "100", description = "%cache-dir-clean-up-frequency-help",
+		name = "%cache-dir-clean-up-frequency", required = false
+	)
 	public int cacheDirCleanUpFrequency();
 
 	@Meta.AD(required = false)
