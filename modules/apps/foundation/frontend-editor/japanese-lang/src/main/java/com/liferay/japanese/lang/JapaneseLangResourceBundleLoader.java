2ba899f5f79631273d187e8a1e8b683d0cbdaf0f
Jonathan McCann <jonathan.mccann@liferay.com>
2016-07-14 09:34:22 -0700

    Japanese Resource Bundle Loader

---
+++ modules/apps/foundation/frontend-editor/japanese-lang/src/main/java/com/liferay/japanese/lang/JapaneseLangResourceBundleLoader.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.japanese.lang;
+
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.ResourceBundleLoader;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+
+import java.util.ResourceBundle;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Carlos Sierra Andrés
+ */
+@Component(
+	immediate = true,
+	property = {
+		"bundle.symbolic.name=com.liferay.portal.settings.web",
+		"resource.bundle.base.name=content.Language",
+		"servlet.context.name=portal-settings-web"
+	}
+)
+public class JapaneseLangResourceBundleLoader
+	implements ResourceBundleLoader {
+
+	@Override
+	public ResourceBundle loadResourceBundle(String languageId) {
+		Class<?> clazz = getClass();
+
+		ClassLoader classLoader = clazz.getClassLoader();
+
+		return ResourceBundleUtil.getBundle(
+			"content.Language", LocaleUtil.fromLanguageId(languageId),
+			classLoader);
+	}
+
+}
