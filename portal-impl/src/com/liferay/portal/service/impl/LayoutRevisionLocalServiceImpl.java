3039414ef30ce2d0cbecda04fa9fc277d14d5b0e
ChrisKian <christopher.kian@liferay.com>
2017-10-27 18:01:46 -0700

    LPS-75562 Only use the ID in threadLocal if it's the correct layoutRevision

--- portal-impl/src/com/liferay/portal/service/impl/LayoutRevisionLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/LayoutRevisionLocalServiceImpl.java
@@ -439,6 +439,17 @@ public class LayoutRevisionLocalServiceImpl
 
 		LayoutRevision layoutRevision = null;
 
+		if (_layoutRevisionId.get() > 0) {
+			LayoutRevision threadLayoutRevision =
+				layoutRevisionPersistence.findByPrimaryKey(
+					_layoutRevisionId.get());
+
+			if (threadLayoutRevision.getParentLayoutRevisionId() ==
+				oldLayoutRevision.getLayoutRevisionId()) {
+				layoutRevision = threadLayoutRevision;
+			}
+		}
+
 		int workflowAction = serviceContext.getWorkflowAction();
 
 		boolean revisionInProgress = ParamUtil.getBoolean(
@@ -446,7 +457,7 @@ public class LayoutRevisionLocalServiceImpl
 
 		if (!MergeLayoutPrototypesThreadLocal.isInProgress() &&
 			(workflowAction != WorkflowConstants.ACTION_PUBLISH) &&
-			(_layoutRevisionId.get() <= 0) && !revisionInProgress) {
+			(layoutRevision == null) && !revisionInProgress) {
 
 			long newLayoutRevisionId = counterLocalService.increment();
 
@@ -497,11 +508,7 @@ public class LayoutRevisionLocalServiceImpl
 				layoutRevision.getPlid(), layoutRevision.getLayoutRevisionId());
 		}
 		else {
-			if (_layoutRevisionId.get() > 0) {
-				layoutRevision = layoutRevisionPersistence.findByPrimaryKey(
-					_layoutRevisionId.get());
-			}
-			else {
+			if (layoutRevision == null) {
 				layoutRevision = oldLayoutRevision;
 			}
 
