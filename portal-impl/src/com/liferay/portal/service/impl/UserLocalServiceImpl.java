f76afbd14bae8b2b662d7561ad6501401d4ea67f
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2018-09-14 13:26:48 +0200

    LPS-85179 Simplify

--- portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
@@ -5244,14 +5244,7 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 
 		user.setStatus(status);
 
-		try {
-			ServiceContextThreadLocal.pushServiceContext(serviceContext);
-
-			user = userPersistence.update(user);
-		}
-		finally {
-			ServiceContextThreadLocal.popServiceContext();
-		}
+		user = userPersistence.update(user, serviceContext);
 
 		reindex(user);
 
