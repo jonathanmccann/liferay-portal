5f0b5716b069204d77b7f906b28a433fdf246595
Brian I. Kim <il.kim@liferay.com>
2019-01-25 12:19:41 -0800

    LPS-86681 PasswordModifiedFilter check is unnecessary during impersonation.

--- portal-impl/src/com/liferay/portal/servlet/filters/password/modified/PasswordModifiedFilter.java
+++ portal-impl/src/com/liferay/portal/servlet/filters/password/modified/PasswordModifiedFilter.java
@@ -69,6 +69,10 @@ public class PasswordModifiedFilter extends BasePortalFilter {
 			return false;
 		}
 
+		if (request.getParameter("doAsUserId") != null) {
+			return false;
+		}
+
 		try {
 			User user = PortalUtil.getUser(request);
 
