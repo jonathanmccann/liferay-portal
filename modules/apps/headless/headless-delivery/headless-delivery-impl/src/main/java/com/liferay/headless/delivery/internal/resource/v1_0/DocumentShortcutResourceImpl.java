dc65172695dde903f705e3eba7a0e515b31b5ce6
Jürgen Kappler <jurgen.kappler@liferay.com>
2024-05-07 16:26:51 +0200

    LPD-25421 Implement getDocumentShortcut endpoint

--- modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/resource/v1_0/DocumentShortcutResourceImpl.java
+++ modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/resource/v1_0/DocumentShortcutResourceImpl.java
@@ -5,9 +5,24 @@
 
 package com.liferay.headless.delivery.internal.resource.v1_0;
 
+import com.liferay.document.library.kernel.model.DLFileEntry;
+import com.liferay.document.library.kernel.service.DLAppService;
+import com.liferay.headless.delivery.dto.v1_0.Document;
+import com.liferay.headless.delivery.dto.v1_0.DocumentShortcut;
+import com.liferay.headless.delivery.dto.v1_0.util.CreatorUtil;
 import com.liferay.headless.delivery.resource.v1_0.DocumentShortcutResource;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.repository.model.FileShortcut;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.vulcan.dto.converter.DTOConverter;
+import com.liferay.portal.vulcan.dto.converter.DTOConverterRegistry;
+import com.liferay.portal.vulcan.dto.converter.DefaultDTOConverterContext;
+
+import java.util.HashMap;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -19,4 +34,63 @@ import org.osgi.service.component.annotations.ServiceScope;
 )
 public class DocumentShortcutResourceImpl
 	extends BaseDocumentShortcutResourceImpl {
+
+	@Override
+	public DocumentShortcut getDocumentShortcut(Long documentShortcutId)
+		throws Exception {
+
+		FileShortcut fileShortcut = _dlAppService.getFileShortcut(
+			documentShortcutId);
+
+		FileEntry fileEntry = _dlAppService.getFileEntry(
+			fileShortcut.getToFileEntryId());
+
+		DefaultDTOConverterContext defaultDTOConverterContext =
+			new DefaultDTOConverterContext(
+				contextAcceptLanguage.isAcceptAllLanguages(), new HashMap<>(),
+				_dtoConverterRegistry, fileShortcut.getFileShortcutId(),
+				contextAcceptLanguage.getPreferredLocale(), contextUriInfo,
+				contextUser);
+
+		return new DocumentShortcut() {
+			{
+				setCreator(
+					() -> CreatorUtil.toCreator(
+						defaultDTOConverterContext, _portal,
+						_userLocalService.fetchUser(fileShortcut.getUserId())));
+				setDateCreated(fileShortcut::getCreateDate);
+				setDateModified(fileShortcut::getModifiedDate);
+				setFolderId(fileShortcut::getFolderId);
+				setReferencedDocument(() -> _toDocument(fileEntry));
+				setSiteId(fileShortcut::getGroupId);
+			}
+		};
+	}
+
+	private Document _toDocument(FileEntry fileEntry) throws Exception {
+		return _documentDTOConverter.toDTO(
+			new DefaultDTOConverterContext(
+				contextAcceptLanguage.isAcceptAllLanguages(), new HashMap<>(),
+				_dtoConverterRegistry, fileEntry.getFileEntryId(),
+				contextAcceptLanguage.getPreferredLocale(), contextUriInfo,
+				contextUser));
+	}
+
+	@Reference
+	private DLAppService _dlAppService;
+
+	@Reference(
+		target = "(component.name=com.liferay.headless.delivery.internal.dto.v1_0.converter.DocumentDTOConverter)"
+	)
+	private DTOConverter<DLFileEntry, Document> _documentDTOConverter;
+
+	@Reference
+	private DTOConverterRegistry _dtoConverterRegistry;
+
+	@Reference
+	private Portal _portal;
+
+	@Reference
+	private UserLocalService _userLocalService;
+
 }
