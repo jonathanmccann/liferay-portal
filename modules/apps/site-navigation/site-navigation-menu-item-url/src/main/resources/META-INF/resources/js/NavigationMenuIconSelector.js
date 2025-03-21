a22c9a609d0089502e303b76498a9c8469c954a7
Jonathan McCann <jonathan.mccann@liferay.com>
2025-03-21 11:04:39 -0700

    Update

---
+++ modules/apps/site-navigation/site-navigation-menu-item-url/src/main/resources/META-INF/resources/js/NavigationMenuIconSelector.js
@@ -0,0 +1,38 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import ClayIcon from '@clayui/icon';
+import ClayPopover from '@clayui/popover';
+import React, {useState} from 'react';
+
+export default function NavigationMenuIconSelector() {
+	const [openPopover, setOpenPopover] = useState(false);
+
+	return (
+		<ClayPopover
+			alignPosition="bottom-right"
+			className="lfr-lock-error-indicator"
+			header={Liferay.Language.get('autosave-error')}
+			onShowChange={setOpenPopover}
+			show={openPopover}
+			trigger={
+				<ClayIcon
+					className="ml-2 mt-0"
+					onClick={() => {
+						setOpenPopover((open) => !open);
+					}}
+					small="true"
+					symbol="exclamation-full"
+				/>
+			}
+		>
+			<div>
+				{Liferay.Language.get(
+					'there-was-an-issue-completing-the-autosave.-this-may-be-due-to-missing-mandatory-fields-or-server-problems.-please-check-and-try-again'
+				)}
+			</div>
+		</ClayPopover>
+	);
+}
--- modules/apps/site-navigation/site-navigation-menu-item-url/src/main/resources/META-INF/resources/js/index.js
+++ modules/apps/site-navigation/site-navigation-menu-item-url/src/main/resources/META-INF/resources/js/index.js
@@ -3,16 +3,4 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {ClayButton} from '@clayui/button';
-import {ClayInput} from '@clayui/form';
-import React from 'react';
-
-export default function NavigationMenuIconSelector() {
-	return (
-		<ClayInput.GroupItem shrink>
-			<ClayButton displayType="secondary" monospaced size="sm">
-				<div id="test">{Liferay.Language.get('minimize')}</div>
-			</ClayButton>
-		</ClayInput.GroupItem>
-	);
-}
+export {default as NavigationMenuIconSelector} from './NavigationMenuIconSelector';
