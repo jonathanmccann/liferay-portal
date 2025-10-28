a0c2c67c1f8b0e710cf0c0f63486639b57fc4ae4
Jonathan McCann <jonathan.mccann@liferay.com>
2025-10-28 13:54:20 -0700

    Remove other test to see if causing issues

--- modules/apps/friendly-url/friendly-url-test/src/testIntegration/java/com/liferay/friendly/url/portlet/test/LayoutFriendlyURLSeparatorCompositeTest.java
+++ modules/apps/friendly-url/friendly-url-test/src/testIntegration/java/com/liferay/friendly/url/portlet/test/LayoutFriendlyURLSeparatorCompositeTest.java
@@ -73,11 +73,11 @@ public class LayoutFriendlyURLSeparatorCompositeTest {
 		_urlSeparators.remove();
 	}
 
-	@Test
+	/*@Test
 	public void testGetLayoutFriendlyURLSeparatorComposite() throws Exception {
 		_assertGetLayoutFriendlyURLSeparatorComposite(
 			FriendlyURLResolverConstants.URL_SEPARATOR_JOURNAL_ARTICLE);
-	}
+	}*/
 
 	@Test
 	public void testGetLayoutFriendlyURLSeparatorCompositeWithConfiguredURLSeparator()
