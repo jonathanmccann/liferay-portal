69a93f4bd262c2314eabd16e48c49847bdff87e2
Jonathan McCann <jonathan.mccann@liferay.com>
2025-11-20 11:17:54 -0800

    LPD-64764 Automatically prepend the friendly URL with a '/' to make it valid. This matches the behavior seen in the UI

--- modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/resource/v1_0/SiteResourceImpl.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/resource/v1_0/SiteResourceImpl.java
@@ -467,7 +467,7 @@ public class SiteResourceImpl extends BaseSiteResourceImpl {
 			_getTypeSettings(site.getTypeSettings(), null),
 			_isManualMembership(site.getManualMembership()),
 			_getMembershipRestriction(site.getMembershipRestriction()),
-			site.getFriendlyUrlPath(), true, false, _isActive(site.getActive()),
+			_getFriendlyUrlPath(site), true, false, _isActive(site.getActive()),
 			serviceContext);
 
 		LiveUsers.joinGroup(
@@ -509,6 +509,18 @@ public class SiteResourceImpl extends BaseSiteResourceImpl {
 		return HashMapBuilder.put(
 			LocaleUtil.getDefault(), site.getDescription()
 		).build();
+	}
+
+	private String _getFriendlyUrlPath(Site site) {
+		String friendlyUrlPath = site.getFriendlyUrlPath();
+
+		if (Validator.isNotNull(friendlyUrlPath) &&
+			!friendlyUrlPath.startsWith(StringPool.SLASH)) {
+
+			friendlyUrlPath = StringPool.SLASH + friendlyUrlPath;
+		}
+
+		return friendlyUrlPath;
 	}
 
 	private int _getMembershipRestriction(Integer membershipRestriction) {
@@ -768,7 +780,7 @@ public class SiteResourceImpl extends BaseSiteResourceImpl {
 					site.getTypeSettings(), group.getTypeSettingsProperties()),
 				_isManualMembership(site.getManualMembership()),
 				_getMembershipRestriction(site.getMembershipRestriction()),
-				site.getFriendlyUrlPath(), false, _isActive(site.getActive()),
+				_getFriendlyUrlPath(site), false, _isActive(site.getActive()),
 				_getServiceContext());
 
 			LiveUsers.joinGroup(
